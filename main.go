50b6c767a0c0990e87083420affb6ef71ee321de
OrtizLin <chenghsien852@gmail.com>
2018-03-21 15:29:21 +0800

    query another url

--- main.go
+++ main.go
@@ -170,7 +170,7 @@ func CrawlAndSent() {
 					date := s.Find("span.updateon").Text()
 					result := strings.Replace(date, "更新于：", "", -1)[:10]
 					fmt.Println("找到關於 " + title + " 的資料, 更新時間為 : " + result)
-					if title == "约定的梦幻岛" || title == "一拳超人" || title == "进击的巨人" || title == "ONE PIECE航海王" || title == "Dr.STONE" || title == "猎人" {
+					if title == "约定的梦幻岛" || title == "一拳超人" || title == "进击的巨人" || title == "ONE PIECE航海王" || title == "Dr.STONE" || title == "猎人" || title == "Summer time rendring" {
 						if result == time_one.Format("2006-01-02") {
 							fmt.Println(title + "在近日內有更新！！")
 							comic.Title = title
