68b8d78545170cd8719565d614cb14c9fdbbdcfa
Otis-TWDD <otis@twdd.com.tw>
2019-05-31 16:49:52 +0800

    testing

--- bot/bot.go
+++ bot/bot.go
@@ -96,7 +96,17 @@ func (app *LineBot) handleText(message *linebot.TextMessage, replyToken string, 
 func (app *LineBot) UserRegister(message string, replyToken string, source *linebot.EventSource) error {
 
 	str := ""
-	if db.CheckRegisteredUser(source.UserID) {
+	userID := source.UserID
+	
+	if userID == "" || userID == nil {
+		userID = source.GroupID
+	}
+
+	if userID == "" || userID == nil {
+		userID = source.RoomID
+	}
+	
+	if db.CheckRegisteredUser(userID) {
 		str = "看看此作者其他的機器人吧！"
 	} else {
 		str = "恭喜您已訂閱連載報報。\n當有最新連載發行時將會第一時間通知您！"
@@ -112,36 +122,36 @@ func (app *LineBot) UserRegister(message string, replyToken string, source *line
 		}
 	}
 
-	if str == "看看此作者其他的機器人吧！" {
-		var columns []*linebot.CarouselColumn
+	// if str == "看看此作者其他的機器人吧！" {
+	// 	var columns []*linebot.CarouselColumn
 
-		column1 := linebot.NewCarouselColumn(
-			"https://i.imgur.com/l3Cdj6B.png", "表特爆報", "只要 PTT 表特版上出現爆文，立即通知使用者，讓你不再錯過精彩文章。",
-			linebot.NewURITemplateAction("立即點我追蹤", "https://line.me/R/ti/p/%40qss3676v"),
-		)
-		column2 := linebot.NewCarouselColumn(
-			"https://i.imgur.com/q7Jq9WK.png", "空汙爆報", "客製化訂閱你家或工作場所附近的空汙觀測站，於每日8點及下午6點進行空汙推播通知。",
-			linebot.NewURITemplateAction("立即點我追蹤", "https://line.me/R/ti/p/%40zhm1865k"),
-		)
-		columns = append(columns, column1)
-		columns = append(columns, column2)
+	// 	column1 := linebot.NewCarouselColumn(
+	// 		"https://i.imgur.com/l3Cdj6B.png", "表特爆報", "只要 PTT 表特版上出現爆文，立即通知使用者，讓你不再錯過精彩文章。",
+	// 		linebot.NewURITemplateAction("立即點我追蹤", "https://line.me/R/ti/p/%40qss3676v"),
+	// 	)
+	// 	column2 := linebot.NewCarouselColumn(
+	// 		"https://i.imgur.com/q7Jq9WK.png", "空汙爆報", "客製化訂閱你家或工作場所附近的空汙觀測站，於每日8點及下午6點進行空汙推播通知。",
+	// 		linebot.NewURITemplateAction("立即點我追蹤", "https://line.me/R/ti/p/%40zhm1865k"),
+	// 	)
+	// 	columns = append(columns, column1)
+	// 	columns = append(columns, column2)
 
-		template := linebot.NewCarouselTemplate(columns...)
-		if _, err := app.bot.ReplyMessage(
-			replyToken,
-			linebot.NewTemplateMessage(str, template),
-		).Do(); err != nil {
-			return err
+	// 	template := linebot.NewCarouselTemplate(columns...)
+	// 	if _, err := app.bot.ReplyMessage(
+	// 		replyToken,
+	// 		linebot.NewTemplateMessage(str, template),
+	// 	).Do(); err != nil {
+	// 		return err
 
-		}
-	} else {
-		if _, err := app.bot.ReplyMessage(
-			replyToken,
-			linebot.NewTextMessage(str),
-		).Do(); err != nil {
-			return err
-		}
-	}
+	// 	}
+	// } else {
+	// 	if _, err := app.bot.ReplyMessage(
+	// 		replyToken,
+	// 		linebot.NewTextMessage(str),
+	// 	).Do(); err != nil {
+	// 		return err
+	// 	}
+	// }
 
 	return nil
 }
